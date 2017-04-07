15af4aee7058dded810880dc72928495c396c65e
CURRYGEDDON <patnamnv@email.wofford.edu>
2017-04-06 21:02:19 -0400

    Changed the way the navbar is going to look like

---
+++ JavaScript/index.html.js
@@ -0,0 +1,11 @@
+var demo = $("div#demoSlider").sliderTabs({
+    indicators: true,
+    panelArrows: true,
+    panelArrowsShowOnHover: true,
+    tabs: false,
+    classes: {
+        panel: 'demoPanel'
+    }
+});/**
+ * Created by Patnamnv on 4/6/17.
+ */
