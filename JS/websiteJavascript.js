c478a028a2fc61a20840138dbcb9930c3bfdc73d
CURRYGEDDON <patnamnv@email.wofford.edu>
2017-08-05 16:04:10 -0400

    better implementation of fade in and fade out... maybe

--- JS/websiteJavascript.js
+++ JS/websiteJavascript.js
@@ -6,10 +6,11 @@ $(document).ready(function(){
 console.log("importing this files works");
     $("div#myNavbar ul li a").click(function(){
         console.log("nav bar clickedddd");
+        $(".newscontainer").fadeOut('slow', function(){
+            var myDiv = $(this).attr("href");
+            $(".newscontainer").replace($(myDiv)).fadeIn('slow');
+        });
 
-        $(".newscontainer").fadeOut('slow');
-        var myDiv = $(this).attr("href");
-        $(myDiv).fadeIn('slow');
     });
 
 
