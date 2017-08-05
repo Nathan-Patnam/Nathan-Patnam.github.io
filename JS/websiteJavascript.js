9c8f691bdecd8e66d55a2b22a553717232f2fcd5
CURRYGEDDON <patnamnv@email.wofford.edu>
2017-08-05 15:06:11 -0400

    Starting to work on tabbed content

--- JS/websiteJavascript.js
+++ JS/websiteJavascript.js
@@ -3,8 +3,19 @@
  * Created by Patnamnv on 8/5/17.
  */
 $(document).ready(function(){
-console.log("importing this files works");
 
+console.log("importing this files works");
+    $("#myNavbar ul li a").click(function(){
+
+        $(".newscontainer").fadeOut('slow', function(){
+            var myDiv = $(this).attr("href");
+        });
+
+        $(myDiv).fadeIn('slow', function(){
+            console.log("yeet");
+        });
+
+    });
 
 
 });
